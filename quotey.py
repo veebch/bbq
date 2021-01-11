4aa32b2f9ee2d87c9e01e0c1897e7db507466063
llvllch <mart@llvll.ch>
2021-01-11 16:19:24 +0000

    Started structure

--- quotey.py
+++ quotey.py
@@ -5,6 +5,8 @@ from PIL import Image, ImageDraw, ImageFont
 from sys import path
 from IT8951 import constants
 import os, random
+import textwrap
+import qrcode
 
 
 def print_system_info(display):
@@ -17,24 +19,70 @@ def print_system_info(display):
     print('  LUT version: {}'.format(epd.lut_version))
     print()
 
+def _place_text(img, text, x_offset=0, y_offset=0):
+    '''
+    Put some centered text at a location on the image.
+    '''
+    fontsize = 350
+
+    draw = ImageDraw.Draw(img)
+
+    try:
+        font = ImageFont.truetype('./fonts/JosefinSans-Light.ttf', fontsize)
+    except OSError:
+        font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSans.ttf', fontsize)
+
+    img_width, img_height = img.size
+    text_width, _ = font.getsize(text)
+    text_height = fontsize
+
+    draw_x = (img_width - text_width)//2 + x_offset
+    draw_y = (img_height - text_height)//2 + y_offset
+
+    draw.text((draw_x, draw_y), text, font=font,fill=(0,0,0) )
+
+def clear_display(display):
+    print('Clearing display...')
+    display.clear()
+
+def newyorkercartoon():
+    print("Get a Cartoon")
+
+def guarianheadlines()
+    print("Get the Headlines")
+
+def wordaday():
+    print("get word a day")
+
+def socialmetrics():
+    print("get social metrics")
+
+def redditquotes():
+    print("get reddit quotes")
+
 def display_image_8bpp(display):
-
-    img_path = 	"images/"+random.choice(os.listdir("images/")) #change dir name to whatever
-    print('Displaying "{}"...'.format(img_path))
-
+    gudepath = 	"gudetama.png"
     # clearing image to white
-    display.frame_buf.paste(0xFF, box=(0, 0, display.width, display.height))
-
-    img = Image.open(img_path)
-
+    clear_display(display)
+#    display.frame_buf.paste(0xFF, box=(0, 0, display.width, display.height))
+    
+    img = Image.new("RGB", (1448, 1072), color = (255, 255, 255) )
+    imggude = Image.open(gudepath)
     # TODO: this should be built-in
     dims = (display.width, display.height)
 
     img.thumbnail(dims)
     paste_coords = [dims[i] - img.size[i] for i in (0,1)]  # align image with bottom of display
+
+    imggude2=imggude.resize((700,500), Image.ANTIALIAS)
+    img.paste(imggude2,(450, 250))
+    sleep(1)
+    _place_text(img,"$34,150",0,350)
+    img=img.rotate(180, expand=True)
     display.frame_buf.paste(img, paste_coords)
 
     display.draw_full(constants.DisplayModes.GC16)
+
 
 def parse_args():
     p = argparse.ArgumentParser(description='Test EPD functionality')
@@ -65,12 +113,12 @@ def main():
 
         print('VCOM set to', display.epd.get_vcom())
 
-        tests += [print_system_info]
+#        tests += [print_system_info]
 
     else:
         from IT8951.display import VirtualEPDDisplay
         display = VirtualEPDDisplay(dims=(800, 600), rotate=args.rotate)
-
+    print_system_info(display)
     display_image_8bpp(display)
     print('Done!')
 
