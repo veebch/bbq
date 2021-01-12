8c8265dd8fbf7f0a36505a8ae351b72d84a05f5f
llvllch <mart@llvll.ch>
2021-01-12 13:49:48 +0000

    some reddit quote formatting

--- quotey.py
+++ quotey.py
@@ -9,7 +9,10 @@ import textwrap
 import qrcode
 import feedparser
 import requests
-
+import textwrap
+import unicodedata
+import re
+import logging
 
 def print_system_info(display):
     epd = display.epd
@@ -21,16 +24,15 @@ def print_system_info(display):
     print('  LUT version: {}'.format(epd.lut_version))
     print()
 
-def _place_text(img, text, x_offset=0, y_offset=0):
+def _place_text(img, text, x_offset=0, y_offset=0,fontsize=40):
     '''
     Put some centered text at a location on the image.
     '''
-    fontsize = 350
 
     draw = ImageDraw.Draw(img)
 
     try:
-        font = ImageFont.truetype('./fonts/JosefinSans-Light.ttf', fontsize)
+        font = ImageFont.truetype('./fonts/JosefinSans-.ttf', fontsize)
     except OSError:
         font = ImageFont.truetype('/usr/share/fonts/TTF/DejaVuSans.ttf', fontsize)
 
@@ -49,21 +51,45 @@ def clear_display(display):
 
 def newyorkercartoon(img):
     print("Get a Cartoon")
-    gudepath =  "gudetama.png"
-    imggude = Image.open(gudepath)
-    imggude2=imggude.resize((200,150), Image.ANTIALIAS)
-    img.paste(imggude2,(100, 100))
-
+    d = feedparser.parse('https://www.newyorker.com/feed/cartoons/daily-cartoon')
+    cartoonurl=d.item[0]
     return img
 
 def guardianheadlines(img):
     print("Get the Headlines")
-    logourl="https://assets.guim.co.uk/images/guardian-logo-rss.c45beb1bafa34b347ac333af2e6fe23f.png"
+
     d = feedparser.parse('https://www.theguardian.com/uk/rss')
+    logourl=d['feed']['image']['href']
+    print(d.url)
     imlogo = Image.open(requests.get(logourl, stream=True).raw)
     img.paste(imlogo,(100, 100))
-    print (d['title'])
+
+
+    text=d.entries[0].title
+    img=writewrappedlines(img,text,80)
+
     return img
+
+
+def writewrappedlines(img,text,fontsize):
+    lines = textwrap.wrap(text, width=27)
+    y_text = -300
+    for line in lines:
+        width, height = 0, 110
+        _place_text(img, line,0, y_text, fontsize)
+        y_text += height
+    return img
+
+def replacenth(string, sub, wanted, n):
+    where = [m.start() for m in re.finditer(sub, string)][n-1]
+    before = string[:where]
+    after = string[where:]
+    after.replace(sub, wanted, 1)
+    newString = before + after
+    return newString
+
+def by_size(words, size):
+    return [word for word in words if len(word) <= size]
 
 def wordaday(img):
     print("get word a day")
@@ -75,7 +101,55 @@ def socialmetrics(img):
 
 def redditquotes(img):
     print("get reddit quotes")
-    _place_text(img,"$34,150",0,350)
+    quoteurl = 'https://www.reddit.com/r/quotes/top/.json?t=week&limit=100'
+    rawquotes = requests.get(quoteurl,headers={'User-agent': 'Chrome'}).json()
+    quotestack = []
+    i=0
+    try:
+        length= len(rawquotes['data']['children'])
+        while i < length:
+            quotestack.append(str(rawquotes['data']['children'][i]['data']['title']))
+            i+=1
+        for key in rawquotes.keys():
+            print(key)
+    except:
+        print('Reddit Does Not Like You')
+
+#   Tidy quotes
+    i=0
+    while i<len(quotestack):
+        result = unicodedata.normalize('NFKD', quotestack[i]).encode('ascii', 'ignore')
+        quotestack[i]=result.decode()
+        i+=1
+    quotestack = by_size(quotestack, 140)
+    
+    while True:
+        quote=random.choice (quotestack)
+        string = quote
+        count = quote.count("\"")
+        print("Count="+str(count))
+        if count == 2:
+            logging.info("Repairing a quotation only Quate")
+            sub = '\"'
+            wanted = '\" ~'
+            n = 2
+            quote=replacenth(quote, sub, wanted, n)
+
+        quote= re.sub("\s+\"\s+", "\"", quote)
+        quote= re.sub("~|-|—|―", "--", quote)
+
+
+        splitquote = quote.split("--")
+        quote = splitquote[0]
+
+        if splitquote[-1]!=splitquote[0]:
+            img=writewrappedlines(img,quote,80)
+            source = splitquote[-1]
+            source = source.strip()
+            print(source)
+            _place_text(img,source,0,380,50)
+            break
+
     return img
 
 def display_image_8bpp(display, img):
@@ -121,7 +195,7 @@ def main():
         from IT8951.display import VirtualEPDDisplay
         display = VirtualEPDDisplay(dims=(800, 600), rotate=args.rotate)
     print_system_info(display)
-    my_list = [guardianheadlines]
+    my_list = [redditquotes]
     clear_display(display)
     img = Image.new("RGB", (1448, 1072), color = (255, 255, 255) )
     img=random.choice(my_list)(img)
